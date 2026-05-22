bfd945c82cd33c0ed6baa20cef659c44830b654e
Pablo Zapata <tamtok2.0@gmail.com>
2026-05-22 00:47:09 -0600

    Fix on tweet service, hardcoded CORS

--- services/tweets-service/index.js
+++ services/tweets-service/index.js
@@ -3,7 +3,6 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // Endpoint: GET /tweets?hashtag=TitansCrews&limit=10
